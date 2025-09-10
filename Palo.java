2e0acd5e4a1d6d794bb65b5c4d18a9293a751fcb
lautaro0910 <lautaronahuelmaida@gmail.com>
2025-09-10 20:10:04 +0000

    Palo

---
+++ Palo.java
@@ -0,0 +1,16 @@
+public class Palo{
+
+      public Palo() {
+    }
+
+    private int[][] forma = {
+        {0, 1, 0},
+        {0, 1, 0},
+        {0, 1, 0},
+        {0, 1, 0}
+    };
+
+    public int[][] getForma() {
+        return forma;
+    }
+}
