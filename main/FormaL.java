e0ba70bd621b06a1d232a9998a3abc65fa69fa39
Juan Bautista Soto <85781797+BautistaSoto06@users.noreply.github.com>
2025-09-10 20:09:46 +0000

    FormaLterminada

--- main/FormaL.java
+++ main/FormaL.java
@@ -1,6 +1,6 @@
 public class FormaL{
 
-    int formaL = new int[3][3] 
+    int formaLleft = new int[3][3] 
     formaL [0][0] = 1;
     formaL [0][1] = 1;
     formaL [0][2] = 1;
@@ -10,5 +10,17 @@ public class FormaL{
     [1] [] []
     [1] [] []
     [1] [1] [] 
+    */
+
+   int formaLRight = new int[3][3] 
+    formaL [2][0] = 1;
+    formaL [2][1] = 1;
+    formaL [2][2] = 1;
+    formaL [1][2] = 1; 
+
+    /* Forma de la L
+    [] [] [1]
+    [] [] [1]
+    [] [1] [1] 
     */
 }
