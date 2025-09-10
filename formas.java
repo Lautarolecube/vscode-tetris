e1327fe1056076bfeb96ad82f08e0f4f4d4fd8f3
Juan Bautista Soto <85781797+BautistaSoto06@users.noreply.github.com>
2025-09-10 19:22:32 +0000

    forma de l

---
+++ formas.java
@@ -0,0 +1,18 @@
+public class formas{
+
+
+
+    int formaL = new int[3][3]
+ 
+    formaL [0][0] = 1;
+    formaL [0][1] = 1;
+    formaL [0][2] = 1;
+    formaL [1][2] = 1; 
+
+    /* Forma de la L
+    [1] [] []
+    [1] [] []
+    [1] [1] [] 
+    */
+
+}
