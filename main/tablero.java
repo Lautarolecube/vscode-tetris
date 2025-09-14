2ac4abb5bab7648e715c8376c57cd17e7fdc4557
Juan Bautista Soto <bautistasoto918@gmail.com>
2025-09-14 10:36:08 -0300

    Agrego de tablero

---
+++ main/tablero.java
@@ -0,0 +1,18 @@
+public class tablero {
+    private final int[][] matrizTablero;
+
+    public tablero() {
+        matrizTablero = new int[20][10];
+    }  
+
+    public int[][] getMatrizTablero() {
+        return matrizTablero;
+    }
+
+    public void setMatrizTablero(int fila, int columna, int valor) {
+        this.matrizTablero[fila][columna] = valor;
+    }
+
+
+
+}
